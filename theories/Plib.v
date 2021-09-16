e1f7bf4f843d6767aff93472abbf4491b183224a
Talia Ringer <talia@dependenttyp.es>
2021-09-16 11:17:28 -0500

    Remove comments

--- theories/Plib.v
+++ theories/Plib.v
@@ -1,32 +1,3 @@
-(* Dependencies for our library *)
-(*Declare ML Module "utilities".
-Declare ML Module "coq".
-Declare ML Module "termutils".
-Declare ML Module "inference".
-Declare ML Module "constants".
-Declare ML Module "representationutils".
-Declare ML Module "state".
-Declare ML Module "envs".
-Declare ML Module "devutils".
-Declare ML Module "hofs".
-Declare ML Module "typesandequality".
-Declare ML Module "debruijn".
-Declare ML Module "hofimpls".
-Declare ML Module "inductive".
-Declare ML Module "contexts".
-Declare ML Module "transformation".
-Declare ML Module "ltac_plugin".
-Declare ML Module "decompiler".*)
-
-(* Dependencies for SerAPI *)
-(*Declare ML Module "sexplib0".
-Declare ML Module "ppx_sexp_conv_lib".
-Declare ML Module "bigarray".
-Declare ML Module "caml".
-Declare ML Module "parsexp".
-Declare ML Module "sexplib".
-Declare ML Module "serlib".*)
-
-(* Our library *)
+(* Uses the ad-hoc .cmxs file *)
 Declare ML Module "plib_full_plugin".
 
